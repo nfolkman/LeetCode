f3828bac7b9dbbd7636d275464018a651feaa68d
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-07-05 22:44:26 -0500

    Easy

---
+++ Contains Duplicate.js
@@ -0,0 +1,35 @@
+/* Description: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+
+
+// Solution 1
+var containsDuplicate = function(nums) {
+    let filteredArr = nums.filter((el,index) => nums.indexOf(el) !== index)
+    if(filteredArr.length > 0){
+    return true
+    } else {
+    return false
+    }
+};
+    
+    // Solution 2
+var containsDuplicate = function(nums) {
+    if(nums.filter((el,index)=> nums.indexOf(el) !== index).length > 0){
+    return true
+    } else {
+return false
+}
+    
+    // Solution 3
+var containsDuplicate = function(nums) {
+    return nums.filter((el,index)=> nums.indexOf(el) !== index).length > 0
+};
+    
+    // Solution 4
+let containsDuplicate = (nums) => nums.filter((el,index)=> nums.indexOf(el) !== index).length > 0
+    
+    
+    /* test cases:
+    containsDuplicate([1,1,1,3,3,4,3,2,4,2])
+    containsDuplicate([1,2,3,1])
+    containsDuplicate([1,2,3,4]) */
