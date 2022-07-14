1281c332048957d1617146a6275490ca363a2aa2
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-07-13 23:39:47 -0500

    Easy

---
+++ Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,21 @@
+/* Description: You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time.
+However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.   */
+
+
+var maxProfit = function(prices) {
+    let profit = 0
+		for(let i = 0; i< prices.length-1; i++){
+			const diff = prices[i+1]-prices[i]
+			if(diff > 0){
+			profit += diff
+		}
+	}
+return profit
+}
+
+
+// no test cases
